1899630860fed0c49dbe5507fdfbabc7e2daa322
ejwu <2218930+ejwu@users.noreply.github.com>
2021-12-28 15:55:57 -0800

    [scripts] Make multiprocessing work

--- scripts/bar_optimizer.py
+++ scripts/bar_optimizer.py
@@ -5,10 +5,12 @@ import json
 import multiprocessing
 import resource
 import sys
+import time
 from collections import Counter
 from collections import defaultdict
 from multiprocessing import Pool
 from multiprocessing import Process
+from multiprocessing import JoinableQueue
 
 # TODO: Update when bar level increases
 MAT_SHOP = {
@@ -193,6 +195,69 @@ class BestDrinkSet:
         if self.comp(drinks, self.best) > 0:
             self.best = drinks
 
+class Stats:
+    def __init__(self):
+        self.bdc_cost = BestDrinkSet(sort_by_cost)
+        self.bdc_fame = BestDrinkSet(sort_by_fame)
+        self.bdc_fame_effic = BestDrinkSet(sort_by_fame_effic)
+        self.bdc_tickets = BestDrinkSet(sort_by_tickets)
+        self.bdc_tickets_effic = BestDrinkSet(sort_by_tickets_effic)
+        self.bdc_overall = BestDrinkSet(sort_by_overall)
+        self.bdc_overall_effic = BestDrinkSet(sort_by_overall_effic)
+        self.num_processed = 0
+
+    def offer_all(self, drinks):
+        self.bdc_cost.offer(drinks)
+        self.bdc_fame.offer(drinks)
+        self.bdc_fame_effic.offer(drinks)
+        self.bdc_tickets.offer(drinks)
+        self.bdc_tickets_effic.offer(drinks)
+        self.bdc_overall.offer(drinks)
+        self.bdc_overall_effic.offer(drinks)
+        self.num_processed += len(drinks)
+
+    def add(self, stats):
+        self.bdc_cost.offer(stats.bdc_cost.best)
+        self.bdc_fame.offer(stats.bdc_fame.best)
+        self.bdc_fame_effic.offer(stats.bdc_fame_effic.best)
+        self.bdc_tickets.offer(stats.bdc_tickets.best)
+        self.bdc_tickets_effic.offer(stats.bdc_tickets_effic.best)
+        self.bdc_overall.offer(stats.bdc_overall.best)
+        self.bdc_overall_effic.offer(stats.bdc_overall_effic.best)
+        self.num_processed += stats.num_processed
+
+    def print(self):
+        print("------------------------------------------")
+        print(f"max cost: {get_drink_set_info(self.bdc_cost.best)[0]}")
+        print_combo(self.bdc_cost.best)
+
+        [c, f, t] = get_drink_set_info(self.bdc_fame.best)
+        print(f"\n\nmax fame: {f}, tickets {t}, cost {c}")
+        print_combo(self.bdc_fame.best)
+
+        [c, f, t] = get_drink_set_info(self.bdc_fame_effic.best)
+        print(f"\n\nmax fame efficiency: {f}, tickets {t}, cost {c}")
+        print_combo(self.bdc_fame_effic.best)
+
+        [c, f, t] = get_drink_set_info(self.bdc_tickets.best)
+        print(f"\n\nmax tickets: {t}, fame {f}, cost {c}")
+        print_combo(self.bdc_tickets.best)
+
+        [c, f, t] = get_drink_set_info(self.bdc_tickets_effic.best)
+        print(f"\n\nmax tickets efficiency: {t}, fame {f}, cost {c}")
+        print_combo(self.bdc_tickets_effic.best)
+
+        [c, f, t] = get_drink_set_info(self.bdc_overall.best)
+        print(f"\n\nmax overall:")
+        print_combo(self.bdc_overall.best)
+
+        [c, f, t] = get_drink_set_info(self.bdc_tickets_effic.best)
+        print(f"\n\nmax overall efficiency:")
+        print_combo(self.bdc_overall_effic.best)
+
+        print(f"\n\n{self.num_processed:,} combos processed")
+        print("------------------------------------------")
+        
 # Drink info is [cost, fame, tickets]
 def get_cost_diff_desc(l_info, r_info):
     return l_info[0] - r_info[0]
@@ -208,6 +273,8 @@ def get_fame_effic_diff(l_info, r_info):
     # The starting empty drink set has no cost
     if r_info[0] == 0:
         return 1
+    if l_info[0] == 0:
+        return -1
     return (l_info[1] / l_info[0]) - (r_info[1] / r_info[0])
 
 # 3.25 is close to the ratio of max_tickets to max_fame at bar level 9
@@ -219,6 +286,8 @@ def get_overall_effic_diff(l_info, r_info):
     # The starting empty drink set has no cost
     if r_info[0] == 0:
         return 1
+    if l_info[0] == 0:
+        return -1
     return ((l_info[1] * OVERALL_COEFF + l_info[2]) / l_info[0]) - ((r_info[1] * OVERALL_COEFF + r_info[2]) / r_info[0])
     
 def get_tickets_diff(l_info, r_info):
@@ -228,6 +297,8 @@ def get_tickets_effic_diff(l_info, r_info):
     # The starting empty drink set has no cost
     if r_info[0] == 0:
         return 1
+    if l_info[0] == 0:
+        return -1
     return (l_info[2] / l_info[0]) - (r_info[2] / r_info[0])
 
 def sort_by_cost(l, r):
@@ -317,36 +388,19 @@ def sort_by_overall_effic(l, r):
     if get_overall_diff(l_info, r_info) != 0:
         return get_overall_diff(l_info, r_info)
     return 0
-    
-bdc_cost = BestDrinkSet(sort_by_cost)
-bdc_fame = BestDrinkSet(sort_by_fame)
-bdc_fame_effic = BestDrinkSet(sort_by_fame_effic)
-bdc_tickets = BestDrinkSet(sort_by_tickets)
-bdc_tickets_effic = BestDrinkSet(sort_by_tickets_effic)
-bdc_overall = BestDrinkSet(sort_by_overall)
-bdc_overall_effic = BestDrinkSet(sort_by_overall_effic)
 
-combos_processed = 0
+stats = Stats()
 
-def process_leaf_nodes(all_c):
-    global combos_processed
+def stats_callback(stats):
+    global results_queue
+    # TODO: how does this even happen?
+    if stats.num_processed > 0:
+        results_queue.put(stats)
 
-    for drinks in all_c:
-        combos_processed += 1
-        if combos_processed % 10000000 == 0:
-            print(f"{combos_processed:,} processed, {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss:,}K memory in use")
-        bdc_cost.offer(drinks)
-        bdc_fame.offer(drinks)
-        bdc_fame_effic.offer(drinks)
-        bdc_tickets.offer(drinks)
-        bdc_tickets_effic.offer(drinks)
-        bdc_overall.offer(drinks)
-        bdc_overall_effic.offer(drinks)
-        
-def all_combos(num_drinks_remaining, drinks_made, drink_set):
+def all_combos_leafy_helper(num_drinks_remaining, drinks_made, drink_set):
     combos = []
     if num_drinks_remaining == 0:
-        return drinks_made
+        return [drinks_made]
     
     minimum = get_drink_set_min(drinks_made)
     for drink in drink_set:
@@ -355,43 +409,66 @@ def all_combos(num_drinks_remaining, drinks_made, drink_set):
             if can_make_drinks(made):
                 combos.append(made)
 
-    if num_drinks_remaining > 1:
-        to_return = []
+    to_return = []
 
+    for combo in combos:
+        to_return += all_combos_leafy_helper(num_drinks_remaining - 1, combo, drink_set)
+
+    return to_return
+
+def all_combos_helper_top_level(num_drinks_remaining, drinks_made, drink_set):
+    stats = Stats()
+    for combo in all_combos_leafy_helper(num_drinks_remaining, drinks_made, drink_set):
+        stats.offer_all(combo)
+
+    return stats
+
+def all_combos(num_drinks_remaining, drinks_made, drink_set, pool, results):
+    combos = []
+    
+    minimum = get_drink_set_min(drinks_made)
+    for drink in drink_set:
+        if drink_to_index[drink] <= minimum:
+            made = drinks_made + (drink,)
+            if can_make_drinks(made):
+                combos.append(made)
+
+    if num_drinks_remaining > 8:
         for combo in combos:
-            all_c = all_combos(num_drinks_remaining - 1, combo, drink_set)
-            to_return += all_c
+            all_combos(num_drinks_remaining - 1, combo, drink_set, pool, results)
+    elif num_drinks_remaining == 8:
+        pool.apply_async(all_combos_helper_top_level, args=(num_drinks_remaining, drinks_made, drink_set), callback=stats_callback)
 
-        return to_return
-    else:
-        process_leaf_nodes(combos)
-        return []
+def results_consumer(queue):
+    calls = 0
+    stats = Stats()
+    while True:
+        if calls % 10000 == 0:
+            print(calls, queue.qsize())
+        result = queue.get()
+        calls += 1
+        if result is not None:
+            stats.add(result)
+            queue.task_done()
+        if result == None:
+            queue.task_done()
+            break
 
-all_combos(bar_level_data[bar_level], (), drink_set)
+    print("from consumer processed", calls)
+    stats.print()
+    print(f"{stats.num_processed:,}")
 
-print(f"\n\nmax cost: {get_drink_set_info(bdc_cost.best)[0]}")
-print_combo(bdc_cost.best)
+pool = Pool(8)
+results_queue = JoinableQueue()
+consumer = Process(target=results_consumer, args=(results_queue, ))
+consumer.start()
 
-[c, f, t] = get_drink_set_info(bdc_fame.best)
-print(f"\n\nmax fame: {f}, tickets {t}, cost {c}")
-print_combo(bdc_fame.best)
+all_combos(bar_level_data[bar_level], (), drink_set, pool, results_queue)
 
-[c, f, t] = get_drink_set_info(bdc_fame_effic.best)
-print(f"\n\nmax fame efficiency: {f}, tickets {t}, cost {c}")
-print_combo(bdc_fame_effic.best)
+pool.close()
+pool.join()
 
-[c, f, t] = get_drink_set_info(bdc_tickets.best)
-print(f"\n\nmax tickets: {t}, fame {f}, cost {c}")
-print_combo(bdc_tickets.best)
+results_queue.join()
 
-[c, f, t] = get_drink_set_info(bdc_tickets_effic.best)
-print(f"\n\nmax tickets efficiency: {t}, fame {f}, cost {c}")
-print_combo(bdc_tickets_effic.best)
-
-[c, f, t] = get_drink_set_info(bdc_overall.best)
-print(f"\n\nmax overall:")
-print_combo(bdc_overall.best)
-
-[c, f, t] = get_drink_set_info(bdc_tickets_effic.best)
-print(f"\n\nmax overall efficiency:")
-print_combo(bdc_overall_effic.best)
+results_queue.put(None)
+consumer.join()
