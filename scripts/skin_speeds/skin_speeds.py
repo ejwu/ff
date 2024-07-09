cf2f48240a2f3371d16dd86cbd6b0db85eb32c4d
ejwu <2218930+ejwu@users.noreply.github.com>
2024-07-09 00:18:43 -0700

    [scripts] Output skin voucher values and fsdb readable data

--- scripts/skin_speeds/skin_speeds.py
+++ scripts/skin_speeds/skin_speeds.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from collections import defaultdict
+from collections import OrderedDict
 import glob
 import json
 import math
@@ -8,7 +9,7 @@ from pathlib import Path
 import pprint
 import sys
 
-PATH_DATE = "ff20230721"
+PATH_DATE = "ff20240529"
 BASE_PATH = Path("/") / "home" / "ejwu" / "ff" / PATH_DATE / "com.egg.foodandroid" / "files"
 PUB_PATH = BASE_PATH / "publish" / "conf" / "en-us"
 RES_PATH = BASE_PATH / "res_sub" / "cards" / "spine" / "effect"
@@ -19,35 +20,57 @@ cardSkins = json.load(open(PUB_PATH / "goods" / "cardSkin.json"))
 fsid_to_name = {}
 skinid_to_name = {}
 spineid_to_name = {}
+skinid_to_value = {}
 fsid_to_skinids = defaultdict(list)
 # Really an ordered set
 fsid_to_spineids = defaultdict(dict)
 
 total_skins = 0
 
-for card in cards:
+for cardId, card in cards.items():
     
-    if len(cards[card]["skin"]) > 2:
+    if len(card["skin"]) > 2:
 #        print(cards[card]["name"])
 #        print(card)
 #        print(cards[card]["skin"])
-        total_skins += len(cards[card]["skin"])
-        fsid_to_name[card] = cards[card]["name"]
-        for skins in cards[card]["skin"].values():
+        total_skins += len(card["skin"])
+        fsid_to_name[cardId] = card["name"]
+        for skins in card["skin"].values():
             for skin_id in skins:
-                skinid_to_name[skin_id] = cardSkins[skin_id]["name"]
+                cardSkin = cardSkins[skin_id]
+                skinid_to_name[skin_id] = cardSkin["name"]
                 if not cardSkins[skin_id]["spineId"] in spineid_to_name:
-                    spineid_to_name[cardSkins[skin_id]["spineId"]] = cardSkins[skin_id]["name"]
-                fsid_to_skinids[card].append(skin_id)
-                fsid_to_spineids[card][cardSkins[skin_id]["spineId"]] = None
+                    spineid_to_name[cardSkin["spineId"]] = cardSkin["name"]
+                fsid_to_skinids[cardId].append(skin_id)
+                fsid_to_spineids[cardId][cardSkin["spineId"]] = None
+                if cardSkin["changeGoods"]:
+                    skinid_to_value[skin_id] = cardSkin["changeGoods"][0]["num"]
+                    if cardSkin["changeGoods"][0]["goodsId"] != 890006:
+                        print("weird skin value", cardSkin["name"])
+                        print(cardSkin)
+                    if len(cardSkin["changeGoods"]) != 1:
+                        print(cardSkin)
+                        exit()
+                
 
 #        for skin_id in map(lambda s : next(iter(s)), cards[card]["skin"].values()):
 #            skinid_to_name[skin_id] = cardSkins[skin_id]["name"]
 #            spineid_to_name[cardSkins[skin_id]["spineId"]] = cardSkins[skin_id]["name"]
 #            fsid_to_skinids[card].append(skin_id)
-        if not (RES_PATH / f"{card}.json").exists():
-            print("missing", cards[card]["name"], card, fsid_to_skinids[card])
-
+        if not (RES_PATH / f"{cardId}.json").exists():
+            print("missing", card["name"], cardId, fsid_to_skinids[cardId])
+    else:
+        # No skins, still want to show default
+        fsid_to_name[cardId] = card["name"]
+        print(card["skin"].values())
+        skin_id = next(iter(next(iter(card["skin"].values())).keys()))
+        skinid_to_name[skin_id] = cardSkins[skin_id]["name"]
+        if not cardSkins[skin_id]["spineId"] in spineid_to_name:
+            spineid_to_name[cardSkins[skin_id]["spineId"]] = cardSkins[skin_id]["name"]
+        fsid_to_skinids[cardId].append(skin_id)
+        fsid_to_spineids[cardId][cardSkins[skin_id]["spineId"]] = None
+        if not (RES_PATH / f"{cardId}.json").exists():
+            print("missing", card["name"], cardId, fsid_to_skinids[cardId])
 
             
 #print(fsid_to_name)
@@ -101,7 +124,11 @@ def get_average_time(effects, spine_id):
             splits.append(f"{effect['time']}: {effect['intValue']}%")
 
     if weights != 100 and not weight_error:
-        exit()
+        print(effects)
+        print(weights)
+        print("weights are wonky")
+        splits.append(f" total is {weights}%?!?")
+#        exit()
 
     return ", ".join(splits)
 
@@ -109,28 +136,130 @@ def get_average_time(effects, spine_id):
 # Sake default no weights on basic, probably triggers twice for 200%
 # Almond Tofu skin no time on basic, no heals
 # see "100%?" or "Bugged" for more
-for card in fsid_to_name:
-    print()
-    print(fsid_to_name[card])
-    for spine in fsid_to_spineids[card]:
-#        print(spine)
-        animations = avatar_spine[spine]["animations"]
-        attack = animations["attack"]["duration"]
-        attack_trigger = "Bugged"
-        if "events" in animations["attack"]:
-            attack_trigger = get_average_time(animations["attack"]["events"]["cause_effect"], spine)
-        basic = animations["skill1"]["duration"]
-        basic_trigger = get_average_time(animations["skill1"]["events"]["cause_effect"], spine)
-        energy = animations["skill2"]["duration"]
-        energy_trigger = get_average_time(animations["skill2"]["events"]["cause_effect"], spine)
+num_spines = 0
+
+def print_timings(spine, outfile, skip=False):
+    animations = avatar_spine[spine]["animations"]
+    attack = animations["attack"]["duration"]
+    attack_trigger = "Bugged"
+    if "events" in animations["attack"]:
+        attack_trigger = get_average_time(animations["attack"]["events"]["cause_effect"], spine)
+    basic = animations["skill1"]["duration"]
+    basic_trigger = get_average_time(animations["skill1"]["events"]["cause_effect"], spine)
+    energy = animations["skill2"]["duration"]
+    energy_trigger = get_average_time(animations["skill2"]["events"]["cause_effect"], spine)
+    if not skip:
         print(f"{spineid_to_name[spine]:23} | {attack:8.2f} | {basic:8.2f} | {energy:8.2f} | {attack_trigger:8} | {basic_trigger:8} | {energy_trigger:8}")
+    if outfile:
+        outfile.write(f"{spineid_to_name[spine]:23} | {attack:8.2f} | {basic:8.2f} | {energy:8.2f} | {attack_trigger:8} | {basic_trigger:8} | {energy_trigger:8}\n")
+    return attack, basic
+
+def output_fsdb_row(fsid, spine, outfile):
+#    print("spine is", spine)
+    row = [spine, spineid_to_name[spine], fsid, fsid_to_name[fsid]]
+
+    animations = avatar_spine[spine]["animations"]
+#    print(avatar_spine[spine])
+    row.append(str(animations["attack"]["duration"]))
+    row.append(str(animations["skill1"]["duration"]))
+    row.append(str(animations["skill2"]["duration"]))
+    attack_trigger = "Bugged"
+    if "events" in animations["attack"]:
+        attack_trigger = get_average_time(animations["attack"]["events"]["cause_effect"], spine)
+    row.append(str(attack_trigger))
+    row.append(str(get_average_time(animations["skill1"]["events"]["cause_effect"], spine)))
+    row.append(str(get_average_time(animations["skill2"]["events"]["cause_effect"], spine)))
+#    print("|".join(row))
+    outfile.write("|".join(row))
+    outfile.write("\n")
+
+skin_outputs = open("skin_speeds.txt", "w")
+default_outputs = open("default_speeds.txt", "w")
+voucher_outputs = open("skin_values.txt", "w")
+fsdb_outputs = open("fsdb_data.psv", "w")
+
+print(skinid_to_value)
+
+for card in fsid_to_name:
+    if len(fsid_to_spineids[card]) > 1:
+        print()
+        print(fsid_to_name[card])
+        skin_outputs.write("\n")
+        skin_outputs.write(fsid_to_name[card] + "\n")
+        for spine in fsid_to_spineids[card]:
+            num_spines += 1
+            print_timings(spine, skin_outputs, True)
+            output_fsdb_row(card, spine, fsdb_outputs)
             
 
+        for skinid in fsid_to_skinids[card]:
+            if skinid in skinid_to_value:
+                voucher_outputs.write("\n")
+                voucher_outputs.write(fsid_to_name[card] + "\n")
+                voucher_outputs.write(f"{skinid_to_name[skinid]} {skinid_to_value[skinid]}\n")
+    else:
+        assert len(fsid_to_spineids[card]) == 1, "shouldn't have more than one skin"
+        print()
+        print(fsid_to_name[card])
+        default_outputs.write("\n")
+        default_outputs.write(fsid_to_name[card] + "\n")
+        for spine in fsid_to_spineids[card]:
+            num_spines += 1
+            print_timings(spine, default_outputs, True)
+            output_fsdb_row(card, spine, fsdb_outputs)
+
+skin_outputs.close()
+default_outputs.close()
+voucher_outputs.close()
+fsdb_outputs.close()
+print()
+print(f"{len(fsid_to_name)} FS, {num_spines} spines ({num_spines - len(fsid_to_name)} skins)")
+
+worst_autos = defaultdict(list)
+worst_basics = defaultdict(list)
+
+for card in fsid_to_name:
+    if len(fsid_to_spineids[card]) > 1:
+        print()
+        print(fsid_to_name[card])
+        default_attack = 0
+        default_basic = 0
+        for spine in fsid_to_spineids[card]:
+            skin_name = spineid_to_name[spine]
+            attack, basic = print_timings(spine, None)
+            if "Default" == skin_name:
+                default_attack = attack
+                default_basic = basic
+        # Hack for cases where Default doesn't come first
+        for spine in fsid_to_spineids[card]:
+            skin_name = spineid_to_name[spine]
+            attack, basic = print_timings(spine, None)
+            if "Default" != skin_name:
+                worst_autos[round((float(attack - default_attack)), 3)].append(f"{fsid_to_name[card]}: {skin_name}")
+                worst_basics[round((float(basic - default_basic)), 3)].append(f"{fsid_to_name[card]}: {skin_name}")
+
+                print()
+print("Worst autoattack slowdowns")
+for i in list(reversed(sorted(worst_autos.items())))[:15]:
+    print(i)
+
+print()
+print("Worst basic slowdowns")
+for i in list(reversed(sorted(worst_basics.items())))[:15]:
+    print(i)
+
+print()
+print("Best autoattack speedups")
+for i in list(sorted(worst_autos.items()))[:14]:
+    print(i)
+
+print()
+print("Best basic speedups")
+for i in list(sorted(worst_basics.items()))[:15]:
+    print(i)
 
 
 
 
-
-
-
-
+print(skinid_to_value)
+print(len(skinid_to_value))
